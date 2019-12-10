0f87f967fcba79395dfb3f3bd037ccc2cda756c8
Jeroo97 <jeroo1997@gmail.com>
2019-12-10 12:03:44 -0500

    -Experimenting with the physics constraints.

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SprungWheel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	SetRootComponent(Mass);
+	Mass->SetSimulatePhysics(true);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetSimulatePhysics(true);
+	Wheel->SetupAttachment(Mass);
+
+	MassWheelConstrain = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassWheelConstrain"));
+	MassWheelConstrain->SetupAttachment(Mass);
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
